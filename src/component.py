b81ee53591371dfef14a487d59be16e281944565
ZdenekSrotyr <zdenek.srotyr@keboola.com>
2024-06-14 10:22:55 +0200

    try logging error

--- src/component.py
+++ src/component.py
@@ -358,8 +358,10 @@ class Component(ComponentBase):
 
             reports_2_process.append(dict(profile_id=profile_id, report_id=report_id))
         if errors:
-            logging.exception(UserException, extra={"full_message": '\n'.join(errors), 'error': UserException})
-            # raise UserException('Mismatches in report. More in result.')
+            ue = UserException('Mismatches in report. More in result.')
+            ue.__message__ = '\n'.join(errors)
+            # logging.exception(UserException, extra={"full_message": '\n'.join(errors), 'error': UserException})
+            raise ue
         return reports_2_process
 
     def _get_existing_report(self, profile_id: str, report_id: str) -> CsvReportSpecification:
